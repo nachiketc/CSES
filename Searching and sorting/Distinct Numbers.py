7b02b21c8ea5d194a7857c076fec279fc365cff4
nachiket-chouhan <nachiket.chouhan@salesforce.com>
2023-07-20 22:23:47 +0530

    Searching and Sorting P1

---
+++ Searching and sorting/Distinct Numbers.py
@@ -0,0 +1,16 @@
+
+
+if __name__ == "__main__":
+
+    # # Input from file
+    # with open('CSES/input.txt') as f:
+    #     lines = f.readlines()
+    # print(lines)
+    # n = int(lines[0].rstrip('\n'))
+    # arr = set(lines[1].split(" "))
+
+    # Input from terminal 
+    n = int(input())
+    arr = set(input().split(" "))
+
+    print(len(arr))
